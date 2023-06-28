3c50e8a2fe1b42be81bb2ab625d5ac098fe9ed21
Juliet <juguquro@gmail.com>
2023-06-27 21:43:38 -0600

    hola

--- client/src/pages/Single.jsx
+++ client/src/pages/Single.jsx
@@ -1,37 +1,79 @@
-import React from "react";
-import Edit from '../img/edit.png'
-import Delete from '../img/delete.png'
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import Edit from "../img/edit.png";
+import Delete from "../img/delete.png";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../components/Menu";
+import axios from "axios";
+import moment from "moment";
+import { useContext } from "react";
+import { AuthContext } from "../context/authContext";
+import DOMPurify from "dompurify";
+
 const Single = () => {
-    return (
-        <div className="single">
-            <div className="content">
-                <img src="https://healthyfamilyproject.com/wp-content/uploads/2020/05/Strawberries-background.jpg" />
-                <div className="user">
-                    <img src="https://admin.acceleratingscience.com/food/wp-content/uploads/sites/5/2015/08/single_strawberry__isolated_on_a_white_background.jpg" alt="" />
-                    <div className="info">
-                        <span> John </span>
-                        <p>Posted 2 days ago</p>
-                    </div>
-                    <div className="edit">
-                        <Link to={`/write?edit=2`}  >
-                            <img src={Edit} alt="" />
-                        </Link>
+  const [post, setPost] = useState({});
 
-                        <img src={Delete} alt=""/> 
-                    </div>
-                </div>
-<h1>  Lorem ipsum   </h1>
-                <p>
-                Lorem ipsum    Lorem ipsum    Lorem ipsum    Lorem ipsum    Lorem ipsum  
-                </p>
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const postId = location.pathname.split("/")[2];
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/${postId}`);
+        setPost(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [postId]);
+
+  const handleDelete = async ()=>{
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/")
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  const getText = (html) =>{
+    const doc = new DOMParser().parseFromString(html, "text/html")
+    return doc.body.textContent
+  }
+
+  return (
+    <div className="single">
+      <div className="content">
+        <img src={`../upload/${post?.img}`} alt="" />
+        <div className="user">
+          {post.userImg && <img
+            src={post.userImg}
+            alt=""
+          />}
+          <div className="info">
+            <span>{post.username}</span>
+            <p>Posted {moment(post.date).fromNow()}</p>
+          </div>
+          {currentUser.username === post.username && (
+            <div className="edit">
+              <Link to={`/write?edit=2`} state={post}>
+                <img src={Edit} alt="" />
+              </Link>
+              <img onClick={handleDelete} src={Delete} alt="" />
             </div>
-          <Menu/> 
-
+          )}
         </div>
-
-    );
+        <h1>{post.title}</h1>
+        <p>
+        {post.desc}
+        </p>      </div>
+      <Menu cat={post.cat}/>
+    </div>
+  );
 };
 
 export default Single;
